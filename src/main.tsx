255d1f0c48a177b1847288909467afa951494435
praptij-2267 <prapti.j@somaiya.edu>
2026-08-26 12:49:05 +0530

    Hard fix: Wrap App root with ThemeProvider in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,18 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { I18nextProvider } from 'react-i18next';
 import i18n from './lib/i18n.ts';
+import { ThemeProvider } from './lib/ThemeContext.tsx';
 import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <I18nextProvider i18n={i18n}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <ThemeProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
     </I18nextProvider>
   </StrictMode>
 );
